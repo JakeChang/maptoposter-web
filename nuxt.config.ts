426cdbfc43728f7077552bbd24daa053952ad250
Jake Chang <>
2026-03-23 16:14:07 +0800

    設定開發伺服器固定 port 為 20003

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,9 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  devServer: {
+    port: 20003,
+  },
   vite: {
     plugins: [tailwindcss()],
   },
